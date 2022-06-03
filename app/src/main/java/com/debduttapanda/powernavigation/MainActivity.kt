fd304b2a93f239991981d336b84ef3fef82dea55
Debdutta Panda <bedmad82@gmail.com>
2022-06-03 09:21:14 +0530

    navigation for result

--- app/src/main/java/com/debduttapanda/powernavigation/MainActivity.kt
+++ app/src/main/java/com/debduttapanda/powernavigation/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.debduttapanda.powernavigation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -10,9 +11,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.sp
@@ -69,6 +72,26 @@ class MainActivity : ComponentActivity() {
 fun PageA(navController: NavHostController) {
     var money by remember { mutableStateOf(0) }
     var sendBonus by remember { mutableStateOf(false) }
+    //there are two major way to get the result back
+    //1) observe as state
+    val result = navController
+        .currentBackStackEntry
+        ?.savedStateHandle
+        ?.getLiveData<Int>("totalReceived")?.observeAsState()
+
+    //2) observe inside launched effect
+    //val result = remember { mutableStateOf(0) }
+    //val owner = LocalLifecycleOwner.current
+    /*LaunchedEffect(key1 = Unit){
+        navController
+            .currentBackStackEntry
+            ?.savedStateHandle
+            ?.getLiveData<Int>("totalReceived")
+            ?.observe(owner){
+                Log.d("fdfddffs1",it.toString())
+                result = it
+            }
+    }*/
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -106,8 +129,13 @@ fun PageA(navController: NavHostController) {
             fontWeight = FontWeight.Bold
         )
 
+
         Button(
             onClick = {
+                navController
+                    .currentBackStackEntry
+                    ?.savedStateHandle
+                    ?.remove<Int>("totalReceived")
                 if(!sendBonus){
                     navController.navigate("page_b/$money")
                 }
@@ -121,6 +149,9 @@ fun PageA(navController: NavHostController) {
             )
         ) {
             Text("Send Money")
+        }
+        if((result?.value?:0)>0){
+            Text("Acknowledgement of ${result?.value} received")
         }
     }
 }
@@ -139,6 +170,10 @@ fun PageB(navController: NavHostController, receivedMoney: Int?, bonus: Int?) {
         )
         Button(
             onClick = {
+                navController
+                    .previousBackStackEntry
+                    ?.savedStateHandle
+                    ?.set("totalReceived",(receivedMoney?:0)+(bonus?:0))
                 navController.navigateUp()
             },
             colors = ButtonDefaults.buttonColors(
